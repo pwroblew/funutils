f88b93c7ad53833adf6447a7aea451f4be9d2b32
Paweł Wróblewski <vrobel@gmail.com>
2022-06-18 23:12:12 +0200

    making exported functions public

--- src/main/java/org/pwroblew/javapurefun/funutils/FunUtils.java
+++ src/main/java/org/pwroblew/javapurefun/funutils/FunUtils.java
@@ -9,11 +9,11 @@ import java.util.function.Predicate;
 
 public class FunUtils {
 
-    static <T, V> Function<T, Optional<V>> funSwitch(List<Tuple2<Predicate<T>, Function<T, V>>> list1) {
+    public static <T, V> Function<T, Optional<V>> funSwitch(List<Tuple2<Predicate<T>, Function<T, V>>> list1) {
         return t -> list1.stream().dropWhile(tuple1 -> tuple1._1().negate().test(t)).findFirst().map(tuple -> tuple._2.apply(t));
     }
 
-    static <T, V> Function<T, V> funSwitch2(List<Tuple2<Predicate<T>, Function<T, V>>> list1, Function<T, V> v2) {
+    public static <T, V> Function<T, V> funSwitch2(List<Tuple2<Predicate<T>, Function<T, V>>> list1, Function<T, V> v2) {
         return t -> funSwitch(list1).andThen(opV -> opV.orElseGet(() -> v2.apply(t))).apply(t);
     }
 
